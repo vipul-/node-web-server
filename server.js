09cbc18bc1a5ecf332240266006c233f334a69bf
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2018-11-20 21:57:45 +0545

    Initial Commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+const app = express();
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile("log.txt", `${log}\n`, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log(log);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render("maintenance");
+});
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+    //res.send("Hello Express!");
+    // res.send({
+    //     name: "Vipul",
+    //     fav_food: ["Pizza", "Burger", "Momo", "Hotdog", "Sandwitch"]
+    // });
+    res.render('index', {
+        pageTitle: "Home Page",
+        welcomeText: "Welcome to some site!"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render("about", {
+        pageTitle: "About page"
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send(
+        {
+            ErrorMessage: "Unable to fulfill request."
+        }
+    );
+});
+
+app.listen(8080, () => {
+    console.log("Server listening on port 8080.");
+});
